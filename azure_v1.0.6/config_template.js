555a9a02d23192ffc36db1845198fc521e2d5fa2
claire <yuechubai@qq.com>
2025-11-13 11:51:13 +0800

    Add files via upload

---
+++ azure_v1.0.6/config_template.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+    AZURE_SUBSCRIPTION_KEY: 'xx',
+    AZURE_SERVICE_REGION: 'eastus2',
+    AZURE_CUSTOM_ENDPOINT_ID: 'xxxx',
+    QWEN_API_KEY:  'sxxx',
+    QWEN_API_URL: 'https://dashscope.aliyuncs.com/compatible-mode/v1'
+};
