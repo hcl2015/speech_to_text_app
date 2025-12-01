6f6019cdd5fa405f1e7c1afc091ca634df10afec
claire <yuechubai@qq.com>
2025-12-01 15:50:05 +0800

    Update Azure environment variable names

--- azure_v1.0.6/api/config/index.js
+++ azure_v1.0.6/api/config/index.js
@@ -2,9 +2,9 @@ module.exports = async function (context, req) {
     context.res = {
         status: 200,
         body: {
-            azureSubscriptionKey: process.env.SPEECH_SUBSCRIPTION_KEY,
-            azureServiceRegion: process.env.SPEECH_SERVICE_REGION,
-            azureCustomEndpointId: process.env.CUSTOM_ENDPOINT_ID,
+            azureSubscriptionKey: process.env.AZURE_SUBSCRIPTION_KEY,
+            azureServiceRegion: process.env.AZURE_SERVICE_REGION,
+            azureCustomEndpointId: process.env.AZURE_CUSTOM_ENDPOINT_ID,
             qwenApiKey: process.env.QWEN_API_KEY,
             qwenApiUrl: process.env.QWEN_API_URL
         },
