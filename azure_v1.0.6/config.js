6a537da36f5bb34dc9709f8c4be6d650b0466c01
claire <yuechubai@qq.com>
2025-12-01 23:20:01 +0800

    Update config.js

--- azure_v1.0.6/config.js
+++ azure_v1.0.6/config.js
@@ -3,9 +3,6 @@
 // Prefer storing real keys only in Azure settings and keeping this file local/uncommitted.
 
 const CONFIG = {
-    AZURE_SUBSCRIPTION_KEY: '<YOUR_AZURE_SUBSCRIPTION_KEY>',
     AZURE_SERVICE_REGION: 'eastus',
-    AZURE_CUSTOM_ENDPOINT_ID: '<YOUR_AZURE_CUSTOM_ENDPOINT_ID>',
-    QWEN_API_KEY: '<YOUR_QWEN_API_KEY>',
     QWEN_API_URL: 'https://dashscope.aliyuncs.com/compatible-mode/v1/chat/completions'
 };
